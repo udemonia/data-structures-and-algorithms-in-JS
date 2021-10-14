a1deff4aca75f47a98c7135916c413849165aa24
udemonia <brandon.l.lambert@icloud.com>
2021-10-14 05:54:09 -0500

    twoSum added and solved

---
+++ codingChallenges/twoSum.js
@@ -0,0 +1,29 @@
+
+/*
+?   Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+?   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+?   You can return the answer in any order.
+*/
+
+var twoSum = function(nums, target) {
+    const result = []
+    for (let i = 0; i < nums.length; i ++) {
+        for (let j = i + 1; j < nums.length; j ++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i)
+                result.push(j)
+            }
+        }
+    }
+    return result
+
+};
+
+
+/* 
+
+!   Runtime: 116 ms, faster than 44.73% of JavaScript online submissions for Two Sum.
+!   Memory Usage: 39.7 MB, less than 59.92% of JavaScript online submissions for Two Sum.
+*/
